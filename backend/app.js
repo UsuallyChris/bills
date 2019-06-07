5533445d6e929294622ab8b7514a07f3578f3258
UsuallyChris <schoonc@gmail.com>
2019-06-07 00:52:31 -0700

    Added dotenv as dependency and created connection to mongodb database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// dotenv
+dotenv.config();
+
+// Mongoose Connection
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true},
+  () => console.log('connected to db')
+);
 
 // Router Imports
 const bills = require('./routes/bills');
