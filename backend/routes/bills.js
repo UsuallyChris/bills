588d82ca50703a9657fe7b17b6483302ecc68f6b
UsuallyChris <schoonc@gmail.com>
2019-06-07 18:53:53 -0700

    PUT route done for updating a specific bill. CRUD routing done for bills model

--- backend/routes/bills.js
+++ backend/routes/bills.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { billPostValidation } = require('../validation');
+const { billPostValidation, billPutValidation } = require('../validation');
 
 // Bill model import
 const Bill = require('../models/Bill');
 
 // GET all bills
-router.get('/', (req, res) => {
+router.get('/', (res) => {
   Bill.find()
     .sort({ due_date: 1 })
     .then(bills => res.json(bills))
@@ -37,17 +37,27 @@ router.post('/', async (req, res) => {
 router.get('/:id', (req, res) => {
   Bill.findById(req.params.id)
     .then(bill => res.json(bill))
-    .catch(err => res.json(err))
+    .catch(err => res.send(err));
 })
 
 // PUT update bill
-router.put('/:id', (req, res) => {
-  res.send('PUT update bill');
+router.put('/:id', async (req, res) => {
+
+  const { error } = billPutValidation(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  };
+
+  await Bill.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(bill => res.json(bill))
+    .catch(err => res.send(err));
 });
 
 // DELETE bill
 router.delete('/:id', (req, res) => {
-  res.send('DELETE bill');
+  Bill.findByIdAndDelete(req.params.id)
+    .then(bill => res.json(bill))
+    .catch(err => res.json(err));
 });
 
 module.exports = router;
--- backend/validation.js
+++ backend/validation.js
@@ -3,11 +3,21 @@ const Joi = require('@hapi/joi');
 const billPostValidation = (data) => {
   const schema = {
     name: Joi.string().min(1).required(),
-    due_date: Joi.date().min('now').iso(),
-    amount_due: Joi.number().precision(2).positive()
+    due_date: Joi.date().min('now').iso().required(),
+    amount_due: Joi.number().precision(2).positive().required()
   }
   return Joi.validate(data, schema)
 }
 
+const billPutValidation = (data) => {
+  const schema = {
+    name: Joi.string().min(1).required(),
+    due_date: Joi.date().min('now').iso().required(),
+    amount_due: Joi.number().precision(2).positive().required()
+  }
+  return Joi.validate(data, schema);
+}
+
 module.exports.billPostValidation = billPostValidation;
+module.exports.billPutValidation = billPutValidation;
 
