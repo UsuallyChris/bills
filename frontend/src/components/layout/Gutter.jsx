83eb31f5b191b622349fc4ecb1e64ee55d34a9b6
UsuallyChris <schoonc@gmail.com>
2019-06-09 21:14:09 -0700

    Fixed main content area and scrolling gutter working correctly

--- frontend/src/components/layout/Gutter.jsx
+++ frontend/src/components/layout/Gutter.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Gutter() {
   return(
-    <h1>Gutter</h1>
+    <div className="gutter">
+      <h1>Gutter</h1>
+    </div>
   );
 };
 
--- frontend/src/components/layout/Main.jsx
+++ frontend/src/components/layout/Main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Main() {
   return(
-    <h1>Main</h1>
+    <div className="main">
+      <h1>Main</h1>
+    </div>
   );
 };
 
