21b5e5a132b40895b548947e8b8b682267b0da07
UsuallyChris <schoonc@gmail.com>
2019-06-17 22:35:27 -0700

    Changed BillCard to class based component.

--- frontend/src/components/cards/BillCard.jsx
+++ frontend/src/components/cards/BillCard.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function BillCard(props) {
-  return (
-    <div className="bill-card card-shadow">
-      <h2>{props.name}</h2>
-      <p>{props.date_due}</p>
-      <p>{props.amount_due}</p>
-    </div>
-  )
+class BillCard extends Component {
+
+  render() {
+    return (
+      <div className="bill-card card-shadow">
+        <h2>{this.props.name}</h2>
+        <p>{this.props.date_due}</p>
+        <p>{this.props.amount_due}</p>
+      </div>
+    );
+  };
 };
 
 export default BillCard;
