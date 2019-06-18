a3d3b29d161ec8794f10d468dddd165a02217366
UsuallyChris <schoonc@gmail.com>
2019-06-17 22:43:22 -0700

    BillCard updated to have edit and delete buttons.

--- frontend/src/components/cards/BillCard.jsx
+++ frontend/src/components/cards/BillCard.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class BillCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
 
   render() {
     return (
       <div className="bill-card card-shadow">
-        <h2>{this.props.name}</h2>
-        <p>{this.props.date_due}</p>
-        <p>{this.props.amount_due}</p>
+        <div className="card-content">
+          <h2>{this.props.name}</h2>
+          <p>{this.props.date_due}</p>
+          <p>{this.props.amount_due}</p>
+        </div>
+        <div className="card-buttons">
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
       </div>
     );
   };
