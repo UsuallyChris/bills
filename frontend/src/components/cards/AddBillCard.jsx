b9162f9ec30f73c457c7bed65e11f4ab680e5ade
UsuallyChris <schoonc@gmail.com>
2019-07-08 18:20:45 -0700

    Validation working correctly.

--- frontend/src/components/cards/AddBillCard.jsx
+++ frontend/src/components/cards/AddBillCard.jsx
@@ -14,8 +14,12 @@ const BillSchema = Yup.object().shape({
     .min(1, 'Length must be at least one character.')
     .required('Name is required.'),
   date_due: Yup.date()
+    .typeError('Due Date must be a date.')
+    .min(new Date().getFullYear(), 'Due Date must be at least this year.')
+    .max(new Date('December 31, 2999'), 'Due Date must be in this century.')
     .required('Due Date is required.'),
   amount_due: Yup.number()
+    .typeError('Amount Due must be a number.')
     .positive('Amount Due must be a positive number.')
     .required('Amount Due is required.')
 })
@@ -41,21 +45,21 @@ function AddBillCard(props) {
             <div className="add-bill-card add-bill-shadow">
               <Form>
                 <div className="card-content">
-                  <Field type="text" name="name" placeholder="Name" />      
+                  <Field type="text" name="name" placeholder="Name" />
+                  {errors.name && touched.name ? <p>{errors.name}</p> : null} 
                   <Datepicker
                     name='date_due'
                     placeholder='Date Due'
+                    dateFormat='MMMM, dd yyyy'
                     minDate={new Date()}
-                    allowSameDay={true}
-                    dateFormat="MMMM d, yyyy"
-                    strictParsing
                   />
+                  {errors.date_due && touched.date_due ? <p>{errors.date_due}</p> : null}
                   <Field type="text" name="amount_due" placeholder="Amount Due" />
+                  {errors.amount_due && touched.amount_due ? <p>{errors.amount_due}</p> : null}
                 </div>
                 <div className="add-card-buttons">
                   <button type="submit" className="submit-button" disabled={isSubmitting}>Add Bill</button>
                 </div>
-                {errors.name && touched.name ? console.log(errors.name) : null}
               </Form>
           </div>
       )}
