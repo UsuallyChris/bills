c1f4aa34cf5f4908a118b82a598184ae123d8796
UsuallyChris <schoonc@gmail.com>
2019-07-18 23:49:06 -0700

    CardHeader added to CategoryCard.

--- frontend/src/components/cards/CategoryCard.jsx
+++ frontend/src/components/cards/CategoryCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// Component Imports
+import CardHeader from './CardHeader';
+
 function CategoryCard() {
   return (
     <div className="card-100 shadow">
+      <CardHeader heading="Spending By Category:" />
       <h1>Spending By Category</h1>
     </div>
   )
